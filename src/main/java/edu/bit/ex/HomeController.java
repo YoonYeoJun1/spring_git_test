b4fa8661bf6a82005b662013b07832f42bf54480
yyjj00000 <yyjj00000@gmail.com>
2020-01-30 11:15:51 +0900

    a

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -23,7 +23,7 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home2(Locale locale, Model model) {
+	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
@@ -35,5 +35,19 @@ public class HomeController {
 		
 		return "home";
 	}
+	/*
+	 * @RequestMapping(value = "/", method = RequestMethod.GET) public String
+	 * home2(Locale locale, Model model) {
+	 * logger.info("Welcome home! The client locale is {}.", locale);
+	 * 
+	 * Date date = new Date(); DateFormat dateFormat =
+	 * DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+	 * 
+	 * String formattedDate = dateFormat.format(date);
+	 * 
+	 * model.addAttribute("serverTime", formattedDate );
+	 * 
+	 * return "home2"; }
+	 */
 	
 }
